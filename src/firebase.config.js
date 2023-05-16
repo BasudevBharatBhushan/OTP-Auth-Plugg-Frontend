c857e09e39d378a9fb0155300543c7f09447fabe
BasudevBharatBhushan <bharatbhushan173@gmail.com>
2023-05-16 18:24:56 +0530

    minor change in firebase config

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,7 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-import { getDatabase } from "firebase/database";
 import { getFirestore } from "@firebase/firestore";
 
 console.log(process.env.REACT_APP_PROJECT_ID);
@@ -13,16 +11,13 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
-  databaseURL:
-    "https://todolist-bb8ff-default-rtdb.asia-southeast1.firebasedatabase.app",
+
   // measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const analytics = getAnalytics(app);
 const auth = getAuth(app);
-const database = getDatabase(app);
 const db = getFirestore(app);
 export { auth, database, db };
